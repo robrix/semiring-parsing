c947d3e4c47ae4de1622c68da52f989eb61042f2
Rob Rix <rob.rix@me.com>
2020-03-22 19:23:56 -0400

    Define an Indexable instance for Map.

--- src/Data/Map/Modular.hs
+++ src/Data/Map/Modular.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Data.Map.Modular
 ( Map(..)
 ) where
 
+import           Data.Indexable
 import qualified Data.Map as Map
 
 newtype Map k v = Map { getMap :: Map.Map k v }
+
+instance (Ord k, Monoid v) => Indexable k v (Map k v) where
+  m ! k = Map.findWithDefault mempty k (getMap m)
