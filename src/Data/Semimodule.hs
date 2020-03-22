808a8951c3185d0f18609fd90bc75bed36b91345
Rob Rix <rob.rix@me.com>
2020-03-22 18:53:27 -0400

    Define a class for left-semimodules.

--- src/Data/Semimodule.hs
+++ src/Data/Semimodule.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE FunctionalDependencies #-}
 -- | A left- or right-semimodule over a 'Semiring' generalizes the concept of a vector space over a field.
 module Data.Semimodule
-() where
+( LeftSemimodule(..)
+) where
+
+import Data.Semiring
+
+class (Semiring r, Monoid m) => LeftSemimodule r m | m -> r where
+  (><<) :: r -> m -> m
