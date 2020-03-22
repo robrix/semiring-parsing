bde6fa649567964977c5a726ba72751d1f480b48
Rob Rix <rob.rix@me.com>
2020-03-22 18:28:27 -0400

    Define a Unital class.

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -1,6 +1,7 @@
 module Data.Semiring
 ( zero
 , Semiring(..)
+, Unital(..)
 ) where
 
 zero :: Monoid a => a
@@ -10,3 +11,7 @@ zero = mempty
 class Semigroup r => Semiring r where
   (><) :: r -> r -> r
   infixr 7 ><
+
+
+class (Monoid r, Semiring r) => Unital r where
+  one :: r
