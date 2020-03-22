5d0037369ee64b4ba102e1aaa59958378adc0b19
Rob Rix <rob.rix@me.com>
2020-03-22 19:05:08 -0400

    Define a LeftSemimodule instance for Identity.

--- src/Data/Semimodule.hs
+++ src/Data/Semimodule.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 -- | A left- or right-semimodule over a 'Semiring' generalizes the concept of a vector space over a field.
 module Data.Semimodule
 ( LeftSemimodule(..)
 ) where
 
+import Data.Functor.Identity
 import Data.Semiring
 
 -- | Left-semimodules lift a 'Semiring'’s '><' operation to the semimodule’s elements.
@@ -39,3 +41,6 @@ import Data.Semiring
 class (Semiring r, Monoid m) => LeftSemimodule r m | m -> r where
   (><<) :: r -> m -> m
   infixr 7 ><<
+
+instance (Monoid r, Semiring r) => LeftSemimodule r (Identity r) where
+  a ><< b  = (a ><) <$> b
