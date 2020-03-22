947e3b206f62b7ad56c9b10ca5e629912eef0210
Rob Rix <rob.rix@me.com>
2020-03-22 19:02:03 -0400

    Laws.

--- src/Data/Semimodule.hs
+++ src/Data/Semimodule.hs
@@ -7,6 +7,33 @@ module Data.Semimodule
 import Data.Semiring
 
 -- | Left-semimodules lift a 'Semiring'’s '><' operation to the semimodule’s elements.
+--
+-- Decomposition of '><':
+--
+-- @
+-- (a '><' b) '><<' c = a '><<' b '><<' c
+-- @
+--
+-- Left- and right-distributivity over '<>':
+--
+-- @
+-- (a '<>' b) '><<' c = a '><<' c '<>' b '><<' c
+-- @
+-- @
+-- a '><<' (b '<>' c) = a '><<' b '<>' a '><<' c
+-- @
+--
+-- If @r@ is 'Monoid'al, we additionally have 'mempty' as a left-annihilator for '><<':
+--
+-- @
+-- 'mempty' '><<' a = 'mempty'
+-- @
+--
+-- If @r@ is 'Unital', we additionally have 'one' as a left-identity for '><<':
+--
+-- @
+-- 'one' '><<' a = a
+-- @
 class (Semiring r, Monoid m) => LeftSemimodule r m | m -> r where
   (><<) :: r -> m -> m
   infixr 7 ><<
