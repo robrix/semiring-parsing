a2d08d420edacd1422aad004fd597a752c720319
Rob Rix <rob.rix@me.com>
2020-03-22 19:16:27 -0400

    Define a class for singleton constructions.

--- src/Data/Indexable.hs
+++ src/Data/Indexable.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FunctionalDependencies #-}
 module Data.Indexable
 ( Indexable(..)
+, Singleton(..)
 ) where
 
 class Indexable a b x | x -> a b where
@@ -10,3 +11,8 @@ class Indexable a b x | x -> a b where
 
 instance Indexable a b (a -> b) where
   (!) = ($)
+
+
+class Indexable a b x => Singleton a b x where
+  (|->) :: a -> b -> x
+  infixr 2 |->
