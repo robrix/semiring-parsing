ef65d9a65933cbcff89163aeef0399e4b1fc2c3a
Rob Rix <rob.rix@me.com>
2020-03-22 19:51:21 -0400

    Define a LeftSemimodule instance for Trie.

--- src/Data/Functor/Trie.hs
+++ src/Data/Functor/Trie.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE UndecidableInstances #-}
 module Data.Functor.Trie
 ( Trie(..)
 ) where
+
+import Data.Semimodule
+import Data.Semiring
 
 data Trie i a = a :< i (Trie i a)
   deriving (Foldable, Functor, Traversable)
@@ -14,3 +19,6 @@ instance (Semigroup a, Semigroup (i (Trie i a))) => Semigroup (Trie i a) where
 
 instance (Monoid a, Monoid (i (Trie i a))) => Monoid (Trie i a) where
   mempty = mempty :< mempty
+
+instance (Monoid a, Semiring a, Functor i, Monoid (i (Trie i a))) => LeftSemimodule a (Trie i a) where
+  s ><< (h :< t) = s >< h :< fmap (s ><<) t
