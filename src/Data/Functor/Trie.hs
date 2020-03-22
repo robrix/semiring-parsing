9f1219f1547e52026ceac23767b3cbef6b858ad9
Rob Rix <rob.rix@me.com>
2020-03-22 19:45:53 -0400

    Define a Monoid instance for Trie.

--- src/Data/Functor/Trie.hs
+++ src/Data/Functor/Trie.hs
@@ -11,3 +11,6 @@ infixr 5 :<
 
 instance (Semigroup a, Semigroup (i (Trie i a))) => Semigroup (Trie i a) where
   (h1 :< t1) <> (h2 :< t2) = h1 <> h2 :< t1 <> t2
+
+instance (Monoid a, Monoid (i (Trie i a))) => Monoid (Trie i a) where
+  mempty = mempty :< mempty
