f93203d7ea76d4eaa35c359dccc49aeec2064d10
Rob Rix <rob.rix@me.com>
2020-03-22 19:34:39 -0400

    Derive a Foldable instance for Trie.

--- src/Data/Functor/Trie.hs
+++ src/Data/Functor/Trie.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveFunctor #-}
 module Data.Functor.Trie
 ( Trie(..)
 ) where
 
 data Trie i a = a :< i (Trie i a)
-  deriving (Functor)
+  deriving (Foldable, Functor)
