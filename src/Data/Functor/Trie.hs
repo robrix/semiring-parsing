01fe59a95e03c1203ffd9b239e9b1bda4b4d3d64
Rob Rix <rob.rix@me.com>
2020-03-22 20:13:28 -0400

    Reduce the precedence of :<.

--- src/Data/Functor/Trie.hs
+++ src/Data/Functor/Trie.hs
@@ -16,7 +16,7 @@ import Data.Semiring
 data Trie i a = a :< i (Trie i a)
   deriving (Foldable, Functor, Traversable)
 
-infixr 5 :<
+infix 1 :<
 
 instance (Semigroup a, Semigroup (i (Trie i a))) => Semigroup (Trie i a) where
   (h1 :< t1) <> (h2 :< t2) = h1 <> h2 :< t1 <> t2
