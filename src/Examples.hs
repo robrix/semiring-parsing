f3dd17702b834ca4c8a127657052fff36dcbfce3
Rob Rix <rob.rix@me.com>
2020-03-22 20:21:03 -0400

    Define a grammar for the lowercase alphabet.

--- src/Examples.hs
+++ src/Examples.hs
@@ -2,11 +2,16 @@
 module Examples
 ( a
 , b
+, atoz
 ) where
 
+import Data.Foldable (fold)
 import Data.Indexable
 import Data.Semiring
 
 a, b :: (Singleton String b x, Unital b) => x
 a = single "a"
 b = single "b"
+
+atoz :: (Singleton String b x, Unital b, Monoid x) => x
+atoz = fold [ single [c] | c <- ['a'..'z'] ]
