1a1baa13232d5662402eba90dd95bb87065202bc
Rob Rix <rob.rix@me.com>
2020-03-23 00:15:15 -0400

    Generalize a bunch of the Trie instances.

--- src/Data/Functor/Trie.hs
+++ src/Data/Functor/Trie.hs
@@ -27,13 +27,13 @@ instance (Monoid a, Monoid (i (Trie i a))) => Monoid (Trie i a) where
 instance (Monoid a, Semiring a, Functor i, Monoid (i (Trie i a))) => LeftSemimodule a (Trie i a) where
   s ><< (h :< t) = s >< h :< fmap (s ><<) t
 
-instance (Monoid a, Functor i, LeftSemimodule a (i (Trie i a))) => Semiring (Trie i a) where
+instance (Monoid a, Semiring a, Functor i, Monoid (i (Trie i a))) => Semiring (Trie i a) where
   (h :< t) >< q = h ><< q <> (zero :< fmap (>< q) t)
 
-instance (Unital a, Functor i, LeftSemimodule a (i (Trie i a))) => Unital (Trie i a) where
+instance (Unital a, Functor i, Monoid (i (Trie i a))) => Unital (Trie i a) where
   one = one :< zero
 
-instance (Star a, Functor i, LeftSemimodule a (i (Trie i a))) => Star (Trie i a) where
+instance (Star a, Functor i, Monoid (i (Trie i a))) => Star (Trie i a) where
   star (h :< t) = q where q = star h ><< (one :< fmap (>< q) t)
 
 instance (c ~ Key i, Monoid a, Monoid (i (Trie i a)), Singleton c (Trie i a) (i (Trie i a))) => Indexable [c] a (Trie i a) where
