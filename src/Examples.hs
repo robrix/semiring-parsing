23cddd02e093dc0aaf0b5c0737138c61be778d6a
Rob Rix <rob.rix@me.com>
2020-03-22 20:19:54 -0400

    Define a grammar for the letter b.

--- src/Examples.hs
+++ src/Examples.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Examples
 ( a
+, b
 ) where
 
 import Data.Indexable
 import Data.Semiring
 
-a :: (Singleton String b x, Unital b) => x
+a, b :: (Singleton String b x, Unital b) => x
 a = single "a"
+b = single "b"
