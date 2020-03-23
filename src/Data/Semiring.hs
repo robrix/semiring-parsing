4a8e258dc29e02174e09ad1aad86cad3eca426bf
Rob Rix <rob.rix@me.com>
2020-03-23 06:48:54 -0400

    Define the pointwise Star instance for functions.

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -172,6 +172,9 @@ instance (Star a, Star b, Star c) => Star (a, b, c) where
 instance (Star a, Star b, Star c, Star d) => Star (a, b, c, d) where
   star (a, b, c, d) = (star a, star b, star c, star d)
 
+instance Star b => Star (a -> b) where
+  star = fmap star
+
 
 newtype Arith a = Arith { getArith :: a }
   deriving (Bounded, Enum, Eq, Ix, Functor, Num, Ord, Read, Show)
